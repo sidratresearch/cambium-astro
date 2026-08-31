3b35ca933dd67fd4e680c72b69cd23cdf0a78d91
taiwithers <59430904+taiwithers@users.noreply.github.com>
2026-08-31 17:56:51 -0400

    feat: have `cambium-astro` enable stages by default

--- src/cambium_astro/cli.py
+++ src/cambium_astro/cli.py
@@ -1,3 +1,21 @@
-from cambium.cli.cli import app
+"""`cambium-astro` command."""
 
-from . import installed_stages
+from cambium import config
+from cambium.cli.cli import app
+from pydantic import create_model
+
+
+def main() -> None:
+    """Replacement for the `cambium` command which enables astro-specific stages
+    by default."""
+    default_stages = config.FileConfiguration().stages
+    new_default_stages = ["cambium_astro.PreviewFITS2", *default_stages]
+
+    NewFileConfiguration = create_model(
+        "newFileConfiguration",
+        stages=(list[str] | None, new_default_stages),
+        __base__=config.FileConfiguration,
+    )
+    config.FileConfiguration = NewFileConfiguration
+
+    app()
